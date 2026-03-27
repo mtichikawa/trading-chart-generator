08d665d34a062cbe25fc268a70958d2e2b4b3e99
Mike Ichikawa <projects.ichikawa@gmail.com>
2026-03-26 17:20:15 -0700

    Clean up module init formatting

--- src/__init__.py
+++ src/__init__.py
@@ -4,4 +4,3 @@ trading-chart-generator (T2)
 Reads OHLCV data from T1's PostgreSQL database and generates
 mplfinance candlestick PNG charts with JSON metadata sidecars.
 """
-# note: see README for usage examples
